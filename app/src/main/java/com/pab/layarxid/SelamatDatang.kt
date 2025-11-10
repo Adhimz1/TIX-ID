92239858ffdea64e21cbc9d5d7a17d80be6c97a7
Adhimz1 <adhim@domain.com>
2025-11-10 18:13:17 +0700

    WW

--- app/src/main/java/com/pab/layarxid/SelamatDatang.kt
+++ app/src/main/java/com/pab/layarxid/SelamatDatang.kt
@@ -1,25 +1,16 @@
 package com.pab.layarxid
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SelamatDatang : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val mainLayout = findViewById<ConstraintLayout>(R.id.main)
-        mainLayout.setOnClickListener {
-            val intent = Intent(this, SelamatDatang::class.java)
-            startActivity(intent)
-        }
-
+        setContentView(R.layout.activity_selamat_datang)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
